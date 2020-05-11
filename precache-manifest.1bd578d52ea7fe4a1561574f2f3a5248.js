5361a7dc346ea160a9f7516891a0fdaa0616351a
Lenovo <maasa@list.ru>
2020-05-11 12:11:38 +0400

    Updates

---
+++ precache-manifest.1bd578d52ea7fe4a1561574f2f3a5248.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f42e38cd20dd450c4b1e1c30c21bf3",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "77084e03af081f00ae6d",
+    "url": "/social/static/css/main.e837f67e.chunk.css"
+  },
+  {
+    "revision": "4fa2b0e176921f7ffcf9",
+    "url": "/social/static/js/2.417b591e.chunk.js"
+  },
+  {
+    "revision": "ae247c52cb5532e362a980244e834a93",
+    "url": "/social/static/js/2.417b591e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77084e03af081f00ae6d",
+    "url": "/social/static/js/main.fb4036c7.chunk.js"
+  },
+  {
+    "revision": "d1ea2869e7b6c416b1de",
+    "url": "/social/static/js/runtime-main.3bc1334a.js"
+  },
+  {
+    "revision": "306b00fdedfaa4f721a6478646efefd4",
+    "url": "/social/static/media/1421138632_frozen-480x800.306b00fd.jpg"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/social/static/media/1_9EBHIOzhE1XfMYoKz1JcsQ.26ef8bb4.gif"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/social/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/social/static/media/right-arrow.5e5b72cc.svg"
+  }
+]);
